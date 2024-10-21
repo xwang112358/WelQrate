eae2566174b795d8c71ec5e739cf7b85856f2c0f
Xin Wang <xin874@ucsb.edu>
2024-10-20 22:02:30 -0500

    add init

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='WelQrate',  # Replace with your desired package name
+    name='welqrate',  # Replace with your desired package name
     version='0.1.0',  # Version number of your package
-    description='A description of your package',
-    url='https://github.com/xwang38438/WelQrate.git',  # Replace with your GitHub repo URL
-    author='Your Name',  # Your name
+    description='The official implementation of the WelQrate dataset and benchmark',  
+    url='https://github.com/xwang38438/WelQrate.git',  
+    author='Xin Wang',  # Your name
     author_email='xin.wang.1@vanderbilt.edu',  # Your email
     packages=find_packages(),  # Automatically finds all packages in your repo
     install_requires=[],  # List dependencies here if any
@@ -14,5 +14,5 @@ setup(
         'License :: OSI Approved :: MIT License',  # Adjust license if needed
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Python version requirement
+    python_requires='>=3.8',  # Python version requirement
 )
